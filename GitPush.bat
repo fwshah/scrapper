609717d0d877176f85fa0ef6bbe63e18c5aa90fb
fwshah <fwshah@hotmail.com>
2025-07-27 09:34:33 -0600

    new commit

---
+++ GitPush.bat
@@ -0,0 +1,8 @@
+@echo off
+cd /d "%~dp0scrapper"
+streamlit run app.py
+git add .
+git status
+git commit -m "new commit"
+git push
+pause
--- app.py
+++ app.py
@@ -1,2 +1,2 @@
 import streamlit as st
-st.header("Welcome to streamlit - VLAD")
+st.header("Welcome to streamlit - FOFOOOOOOO")
